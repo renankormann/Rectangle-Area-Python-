4924047bbf751e8c6536a8b5cc30f284fe5f37a0
Renan Caleffo Kormann <103371525+renankormann@users.noreply.github.com>
2025-01-09 16:14:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ Created on Jul 10, 2023
 
 @author: renan
 '''
-from src.rectangle import Rectangle
-from src.rectangle import Square
+from rectangle import Rectangle
+from rectangle import Square
+
+#Testing the functions.
 
 rect = Rectangle(5, 10)
 print(rect.get_area())
 rect.set_width(3)
 print(rect.get_perimeter())
 print(rect)
+print(rect.get_amount_inside(2,3))
 
 sq = Square(9)
 print(sq.get_area())
